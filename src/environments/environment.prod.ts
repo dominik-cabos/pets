04f0a1dc89dfe93c8cf22059dfa08a27a0b84c69
Dom <dominik.cabos@gmail.com>
2020-07-25 02:04:52 +0200

    Production build for hosting on Github Pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
   production: true,
+  petApiBaseUrl: 'https://petstore.swagger.io/v2',
+  userApiBaseUrl: 'https://petstore.swagger.io/v2',
+  defaultPetImage: '/assets/default-pet-avatar.png',
 };
